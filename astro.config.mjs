6cf49184fdda777c44715e668ede1bf27af6da31
rudyhuynh <>
2026-01-21 13:38:21 +0700

    rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    assets: "assets",
+  },
   site: "https://vitype.io.vn",
   outDir: "./docs",
   integrations: [react(), sitemap()],
